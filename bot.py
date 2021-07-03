b6418d6a0921d2735a962b999203f7bf74e5f73c
sampathsir <83348785+sampathsir@users.noreply.github.com>
2021-07-03 15:36:38 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -40,7 +40,7 @@ async def tag_check(_, message):
     if await is_admin(message):
        return
     user = message.from_user.id
-    if TAG not in message.from_user.name:
+    if TAG not in message.from_user.first_name:
        await tagcheck.restrict_chat_member(
         message.chat.id,
         user,
